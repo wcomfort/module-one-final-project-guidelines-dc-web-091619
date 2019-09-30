3f28f2e55d15c686f2d9ee9dfd2d5dcaa88d0e5f
github username <github email address>
2019-09-30 15:54:39 -0400

    require all

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 require 'bundler'
 Bundler.require
 
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+ActiveRecord::Base.establish_connection
+(adapter: 'sqlite3', database: 'db/development.db')
+
+
 require_all 'lib'
+require_all 'bin'
+require_all 'db'
