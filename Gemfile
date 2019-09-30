303678012571a2a9c47488d3bae3851d6fc6dc27
github username <github email address>
2019-09-30 16:35:22 -0400

    middle

--- Gemfile
+++ Gemfile
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
-gem "sinatra-activerecord"
-gem "sqlite3", '~> 1.3.6'
-gem "pry"
-gem "require_all"
+# gem "activerecord", "~> 5.2.3", :require => 'active_record'
+# gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
+# gem 'rake'
+# gem 'require_all'
+# gem 'sqlite3'
+# gem 'pry'
+
+gem 'sqlite3', '~> 1.3.6'
+gem 'activerecord'
+gem 'rspec'
+gem 'pry'
+gem 'sinatra-activerecord'
+gem 'rake'
+gem 'database_cleaner'
--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,22 @@
-require 'bundler'
+# require 'bundler'
+# require "bundler/setup"
+# require "sinatra/activerecord"
+# Bundler.require
+
+# ActiveRecord::Base.establish_connection
+# (adapter: "sqlite3", database: 'db/development.db')
+
+# Dir[File.join(File.dirname(__FILE__), "../bin", "*.rb")].each {|f| require f}
+
+# require_all 'lib'
+# require_all 'bin'
+# require_all 'db'
+
+require “bundler/setup”
+require “sinatra/activerecord”
+require ‘ostruct’
+require ‘date’
 Bundler.require
-
-ActiveRecord::Base.establish_connection
-(adapter: 'sqlite3', database: 'db/development.db')
-
-
-require_all 'lib'
-require_all 'bin'
-require_all 'db'
+Dir[File.join(File.dirname(__FILE__), “../app/models”, “*.rb”)].each {|f| require f}
+connection_details = YAML::load(File.open(‘./config/database.yml’))
+ActiveRecord::Base.establish_connection(connection_details)
