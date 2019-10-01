73cd62800ef5acd55666e63cdef9279e5bd9bd1b
Will <comfortwb21@gmail.com>
2019-10-01 10:22:56 -0400

    save

--- Gemfile
+++ Gemfile
@@ -1,13 +1,6 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
-<<<<<<< HEAD
-gem "sinatra-activerecord"
-gem "sqlite3", '~> 1.3.6'
-gem "pry"
-gem "require_all"
-gem "faker"
-=======
 # gem "activerecord", "~> 5.2.3", :require => 'active_record'
 # gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 # gem 'rake'
@@ -22,4 +15,3 @@ gem 'pry'
 gem 'sinatra-activerecord'
 gem 'rake'
 gem 'database_cleaner'
->>>>>>> 303678012571a2a9c47488d3bae3851d6fc6dc27
--- config/environment.rb
+++ config/environment.rb
@@ -8,11 +8,6 @@
 
 # Dir[File.join(File.dirname(__FILE__), "../bin", "*.rb")].each {|f| require f}
 
-<<<<<<< HEAD
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
-require_all 'lib'
-
-=======
 # require_all 'lib'
 # require_all 'bin'
 # require_all 'db'
@@ -25,4 +20,3 @@ Bundler.require
 Dir[File.join(File.dirname(__FILE__), “../app/models”, “*.rb”)].each {|f| require f}
 connection_details = YAML::load(File.open(‘./config/database.yml’))
 ActiveRecord::Base.establish_connection(connection_details)
->>>>>>> 303678012571a2a9c47488d3bae3851d6fc6dc27
