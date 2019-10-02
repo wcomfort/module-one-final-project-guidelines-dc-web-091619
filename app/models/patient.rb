2ee871512d2dd44e83e4eaed2305dd9d7d9a14e2
Will <comfortwb21@gmail.com>
2019-10-02 17:00:22 -0400

    my review method

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -33,4 +33,34 @@ class Patient < ActiveRecord::Base
         end
     end
 
+    def rating_constraint(rating)
+        if rating > 10
+            10
+        elsif rating < 1
+            1
+        else rating 
+        end
+    end
+
+    def edit_review(id)
+        my_review
+        id = gets.chomp.to_i
+        edit = Review.find(id)
+        puts "Do you edit the rating, content or both?"
+        option = gets.chomp.downcase
+        if option == 'rating' || option == 'both'
+            puts "Please update your rating from 1-10"
+            rating = gets.chomp.to_i
+            rating = rating_constraint(rating)
+            edit.update(rating: rating)
+            edit.save
+        end
+        if option == 'content' || option == 'both'
+            puts "Please update your review content."
+            content = gets.chomp
+            edit.update(content: content)
+            edit.save
+        end 
+    end
+
 end
